d4f6098c52dbe6a77d5fae864483fb906c5ad254
valeriaAKG <137521683+valeriaAKG@users.noreply.github.com>
2024-10-18 08:00:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual é o maoir orgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Canadá?',
+    'Ottawa'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual éo planeta mais próximo ao sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Histótia',
+    'Em que ano o Brasil foi descoberto pelos portugueses?',
+    'Em 1500'
+)
